539b9d4c317e15ae0118d27307eb14e8d3f2a685
alireza <comolestyrusistantaneos@gmail.com>
2022-12-19 20:54:10 +0330

    changed app and card code for clearance

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,9 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
-type syn = {
-  score: number;
-  word: string;
-};
+import Card from "./components/card.component";
+
 function App() {
-  const [state, setstate] = useState("");
-  const [syn, setsyn] = useState<syn[]>([]);
-  const [isloadig, setisloading] = useState(false);
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setstate(e.target.value);
-  const fetchsyns = (word: string) => {
-    setisloading(true);
-    fetch(`https://api.datamuse.com/words?rel_syn=${state}`)
-      .then((res) => res.json())
-      .then(setsyn)
-      .then(() => setisloading(false));
-  };
-  const onSubmitHandler = (e: React.FormEvent) => {
-    e.preventDefault();
-    fetchsyns(state);
-  };
-  const onClickSyn = (newWord: string) => {
-    setstate(newWord);
-    fetchsyns(newWord);
-  };
   return (
     <div className="App">
-      <form onSubmit={onSubmitHandler}>
-        <label>word-search</label>
-        <input value={state} onChange={onChangeHandler} id="word-input"></input>
-        <button>SEARCH</button>
-      </form>
-      {isloadig ? (
-        <div>loading...</div>
-      ) : (
-        <ul>
-          {syn.map((s) => (
-            <li key={s.word} onClick={() => onClickSyn(s.word)}>
-              {s.word}
-            </li>
-          ))}
-        </ul>
-      )}
+      <Card />
     </div>
   );
 }
--- src/components/card.component.tsx
+++ src/components/card.component.tsx
@@ -1,10 +1,54 @@
-import FormInput from "./forminput.component";
+import { useState } from "react";
+
+type syn = {
+  score: number;
+  word: string;
+};
+
 const Card = () => {
+  const [word, setWord] = useState("");
+  const [syn, setSyn] = useState<syn[]>([]);
+  const [isloadig, setIsloading] = useState(false);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setWord(e.target.value);
+
+  const fetchsyns = (word: string) => {
+    setIsloading(true);
+    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then((res) => res.json())
+      .then(setSyn)
+      .then(() => setIsloading(false));
+  };
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetchsyns(word);
+  };
+
+  const onClickSyn = (newWord: string) => {
+    setWord(newWord);
+    fetchsyns(newWord);
+  };
+
   return (
-    <>
-      <FormInput word="hi" lable="wow" />
-      <button>search</button>
-    </>
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <label>word-search</label>
+        <input value={word} onChange={onChangeHandler} id="word-input"></input>
+        <button>SEARCH</button>
+      </form>
+      {isloadig ? (
+        <div>loading...</div>
+      ) : (
+        <ul>
+          {syn.map((s) => (
+            <li key={s.word} onClick={() => onClickSyn(s.word)}>
+              {s.word}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 export default Card;
