0a3f91487cc33614f012a750626f2e96541cdef3
alireza <comolestyrusistantaneos@gmail.com>
2022-12-25 20:31:05 +0330

    app modified for migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Card from "./components/card.component";
 import { WordContext } from "./context/word.contex";
 
+export type synType = {
+  score: number;
+  word: string;
+};
+
 function App() {
-  const { word } = useContext(WordContext);
+  const { word, setWord } = useContext(WordContext);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setWord(e.target.value);
+  const [syn, setSyn] = useState<synType[]>([]);
+  const [isloadig, setIsloading] = useState(false);
+
+  const fetchsyns = (word: string) => {
+    setIsloading(true);
+    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      .then((res) => res.json())
+      .then(setSyn)
+      .then(() => setIsloading(false));
+  };
+  const onSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetchsyns(word);
+  };
   return (
     <div className="flex justify-center bg-zinc-100 h-screen items-center">
-      <Card />
+      <div className=" items-center text-center w-36 rounded-bg p-4 m-3">
+        <form onSubmit={onSubmitHandler}>
+          <div className="flex flex-col rounded bg-zinc-200 justify-center">
+            <label>word-search</label>
+            <input
+              className="rounded text-center bg-white text-black"
+              value={word}
+              onChange={onChangeHandler}
+              id="word-input"
+            ></input>
+            <button className="text-center bg-blue-500 rounded">SEARCH</button>
+          </div>
+        </form>
+        {isloadig ? (
+          <div className="bg-red-600">loading...</div>
+        ) : (
+          <Card {...syn} />
+        )}
+      </div>
     </div>
   );
 }
--- src/components/card.component.tsx
+++ src/components/card.component.tsx
@@ -1,65 +1,28 @@
 import { useState, useContext } from "react";
 import { WordContext } from "../context/word.contex";
+import { synType } from "../App";
 
-export type syn = {
-  score: number;
-  word: string;
-};
-
-const Card = () => {
+const Card = (syn: synType[]) => {
   const { word, setWord } = useContext(WordContext);
-  const [syn, setSyn] = useState<syn[]>([]);
-  const [isloadig, setIsloading] = useState(false);
-
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setWord(e.target.value);
-
-  const fetchsyns = (word: string) => {
-    setIsloading(true);
-    fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
-      .then((res) => res.json())
-      .then(setSyn)
-      .then(() => setIsloading(false));
-  };
-  const onSubmitHandler = (e: React.FormEvent) => {
-    e.preventDefault();
-    fetchsyns(word);
-  };
 
   const onClickSyn = (newWord: string) => {
     setWord(newWord);
-    fetchsyns(newWord);
+    fetchsyns(newWord); //TODO add this to contex so we can access it from here!
   };
 
   return (
-    <div className=" items-center text-center w-36 rounded-bg p-4 m-3">
-      <form onSubmit={onSubmitHandler}>
-        <div className="flex flex-col rounded bg-zinc-200 justify-center">
-          <label>word-search</label>
-          <input
-            className="rounded text-center bg-white text-black"
-            value={word}
-            onChange={onChangeHandler}
-            id="word-input"
-          ></input>
-          <button className="text-center bg-blue-500 rounded">SEARCH</button>
-        </div>
-      </form>
-      {isloadig ? (
-        <div className="bg-red-600">loading...</div>
-      ) : (
-        <ul className="rounded p-2 m-2 shadow-sm bg-slate-300">
-          {syn.map((s) => (
-            <li
-              className="p-2 m-2 border-t-2"
-              key={s.word}
-              onClick={() => onClickSyn(s.word)}
-            >
-              {s.word}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div>
+      <ul className="rounded p-2 m-2 shadow-sm bg-slate-300">
+        {syn.map((s) => (
+          <li
+            className="p-2 m-2 border-t-2"
+            key={s.word}
+            onClick={() => onClickSyn(s.word)}
+          >
+            {s.word}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
