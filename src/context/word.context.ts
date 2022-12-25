39679be837f137e4617edcd22c678e56c0c5e854
alireza <comolestyrusistantaneos@gmail.com>
2022-12-25 18:07:03 +0330

    false2

--- src/context/word.context.ts
+++ src/context/word.context.ts
@@ -1,30 +1,9 @@
-import React, { createContext, useReducer } from "react";
+import { createContext, useState } from "react";
 
-const WordContext = createContext([]);
+const WordContext = createContext("");
 
-const WORD_ACTOION_TYPE = {
-  SEARCH_WORD: "SEARCH_WORD",
-  CHANGE_WORD: "CHANGE_WORD",
-  CLICK_WORD: "CLICK_WORD",
-};
 
-const word = "";
-const syn = "";
-const INITIAL_STATE = [{ word, syn }];
-
-const wordReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case WORD_ACTOION_TYPE.SEARCH_WORD:
-      return [...state, payload];
-    case WORD_ACTOION_TYPE.CHANGE_WORD:
-      return [...state];
-    case WORD_ACTOION_TYPE.CLICK_WORD:
-      return [...state, payload];
-  }
-};
-
-export const WordProvider = ({ children }) => {
-  const [{ word, syn }, dispatch] = useReducer(wordReducer, INITIAL_STATE);
-  const;
-};
+const WordProvider = ({children})=>{
+  const [word,setWord] = useState('')
+  return <WordContext.Provider value={word}>{children}<WordContext.Provider>
+}
