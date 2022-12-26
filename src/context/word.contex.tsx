a8adeb77814a46195e232ecb49e985fc3c5205d2
alireza <comolestyrusistantaneos@gmail.com>
2022-12-26 18:03:51 +0330

    start of reducer transformation

--- src/context/word.contex.tsx
+++ src/context/word.contex.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import { createContext, ReactNode, useState, FC } from "react";
 
 export const WordContext = createContext<{
   word: string;
@@ -8,10 +8,28 @@ export const WordContext = createContext<{
   setWord: () => null,
 });
 
-const INITIAL_WORD = "";
+type stateType = {
+  word: string;
+  syn: string[];
+  isLoading: boolean;
+};
+const INITIAL_WORD: stateType = { word: "", syn: [], isLoading: false };
 
-export const WordProvider = ({ children }: any) => {
+const WORD_ACTION_TYPES = {
+  SET_WORD: "SET_WORD",
+  SET_SYN: "SET_SYN",
+  SET_ISlOADING: "SET_ISlOADING",
+};
+
+const wordReducer = (state: stateType, action) => {
+  const { word, syn, isLoading } = state;
+  const { type, payload } = action;
+};
+
+export const WordProvider: FC<ReactNode> = ({ children }: any) => {
   const [word, setWord] = useState(INITIAL_WORD);
   const value = { word, setWord };
   return <WordContext.Provider value={value}>{children}</WordContext.Provider>;
 };
+
+// TODO write with useReducer
