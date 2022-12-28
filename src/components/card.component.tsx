a4c56fb163c359e337c4fe5bbec8f2b7546d290b
alireza <comolestyrusistantaneos@gmail.com>
2022-12-28 23:50:29 +0330

    loading added

--- src/components/card.component.tsx
+++ src/components/card.component.tsx
@@ -4,6 +4,7 @@ import { RootState, useAppDispatch } from "../../store/store";
 
 const Card = () => {
   const dispatch = useAppDispatch();
+  const isLoading = useSelector((state: RootState) => state.word.isLoading);
   const onClickSyn = (newWord: string) => {
     dispatch(setWord(newWord));
     dispatch(fetchSyns(newWord));
@@ -11,17 +12,21 @@ const Card = () => {
   const syn = useSelector((state: RootState) => state.word.syn);
   return (
     <div>
-      <ul className="rounded p-2 m-2 shadow-sm bg-slate-300">
-        {syn.map((s) => (
-          <li
-            className="p-2 m-2 border-t-2"
-            key={s.word}
-            onClick={() => onClickSyn(s.word)}
-          >
-            {s.word}
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <div>LOADING...</div>
+      ) : (
+        <ul className="rounded p-2 m-2 shadow-sm bg-slate-300">
+          {syn.map((s) => (
+            <li
+              className="p-2 m-2 border-t-2"
+              key={s.word}
+              onClick={() => onClickSyn(s.word)}
+            >
+              {s.word}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
