0e64fbf6d65a6a642b1cce2ffedf1e1b8d16f10a
alireza <comolestyrusistantaneos@gmail.com>
2023-01-07 01:09:32 +0330

    fixed starting saga

--- store/slices/word.saga.ts
+++ store/slices/word.saga.ts
@@ -1,23 +1,28 @@
-import { all, call, put, takeEvery } from "redux-saga/effects";
+import { all, call, put, takeEvery } from "typed-redux-saga";
 import { onFetchFail, onFetchStart, onFetchSuccess } from "./word.slice";
-import axios from 'axios'
 
-export function* fetchDatamuse() {
+type sagaActionType = {
+  type: string;
+  payload: any;
+};
+
+export function* fetchDatamuse(action: sagaActionType) {
   try {
-    yield put(onFetchStart());
-    const response = yield call( axios.get, [`https://api.datamuse.com/words?rel_syn=${}`]
+    yield* put(onFetchStart());
+    const response = yield* call(() =>
+      fetch(`https://api.datamuse.com/words?rel_syn=${action.payload}`)
     );
     const fetchPayload = response.json();
-    yield put(onFetchSuccess(fetchPayload));
+    yield* put(onFetchSuccess(fetchPayload));
   } catch (error) {
-    yield put(onFetchFail(error));
+    yield* put(onFetchFail(error as Error));
   }
 }
 
-export function* onFetchWord(): Generator<'word/onFetchStart'> {
-  yield takeEvery("word/onFetchStart", fetchDatamuse);
+export function* onFetchWord() {
+  yield* takeEvery("word/onFetchStart", fetchDatamuse);
 }
 
 export function* wordSaga() {
-  yield all([onFetchWord]);
+  yield* all([onFetchWord]);
 }
