427f1360a84bac062707ade2e7d5702f8ede3085
alireza <comolestyrusistantaneos@gmail.com>
2023-01-12 17:53:00 +0330

    relateslice setup

---
+++ store/slices/relate.slice.ts
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+export type Trelate = {
+  rhyme: string[] | undefined;
+  nrhyme: string[] | undefined;
+  soundsLike: string[] | undefined;
+  partOf: string[] | undefined;
+  trigger: string[] | undefined;
+};
+
+type relateState = {
+  word: string;
+  relates: Trelate;
+  isOpen: boolean;
+};
+
+type TsetWordAction = {
+  type: string;
+  payload: string;
+};
+type TsetIsOpen = {
+  type: string;
+  payload: boolean;
+};
+type TsetRelateAction = {
+  type: string;
+  payload: Trelate;
+};
+
+const relateSlice = createSlice({
+  name: "relate",
+  initialState: {} as relateState,
+  reducers: {
+    setRelateWord: (state: relateState, action: TsetWordAction) => {
+      state.word = action.payload;
+    },
+    setIsOpen: (state: relateState, action: TsetIsOpen) => {
+      state.isOpen = !state.isOpen;
+    },
+    setRelateFetch: (state: relateState, action: TsetRelateAction) => {
+      state.relates = action.payload;
+    },
+  },
+});
+
+const { actions, reducer } = relateSlice;
+export const { setRelateWord, setIsOpen, setRelateFetch } = actions;
+
+const relateReducer = reducer;
+export default relateReducer;
