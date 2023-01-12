41849bde86c1d1d329dede0e029e76fc7a79e2d5
alireza <comolestyrusistantaneos@gmail.com>
2023-01-12 15:24:57 +0330

    fixed parallel flow

--- store/slices/relate.saga.ts
+++ store/slices/relate.saga.ts
@@ -1,30 +1,27 @@
 import { AnyAction } from "@reduxjs/toolkit";
-import { call, fork, put, spawn, takeLatest } from "typed-redux-saga";
+import { all, call, fork, put, spawn, takeLatest } from "typed-redux-saga";
 
 function* fetches(endPoint: string, payload: string) {
-  const resoponse = yield* call(() =>
+  const resoponse = yield call(() =>
     fetch(`https://api.datamuse.com/words?${endPoint}=${payload}`)
   );
-  yield* call(() => resoponse.json());
+  yield call(() => resoponse.json());
 }
 
 function* fetchRelated(action: AnyAction) {
   const payload = action.payload;
   try {
-    const rhyme = yield* fork(() => fetches("rel_rhy", payload));
-    const apRhyme = yield* fork(() => fetches("rel_nry)", payload));
-    const soundsLike = yield* fork(() => fetches("rel_hom", payload));
-    const partOf = yield* fork(() => fetches("rel_par", payload));
-    const trigger = yield* fork(() => fetches("rel_trg", payload));
+    const answer = yield* all([
+      call(fetches, "rel_rhy", payload),
+      call(fetches, "rel_nry", payload),
+      call(fetches, "rel_hom", payload),
+      call(fetches, "rel_par", payload),
+      call(fetches, "rel_trg", payload),
+    ]);
   } catch (error) {}
 }
 
-function* fetchExtas(action: AnyAction) {
-  const { rhyme, apRhyme, soundsLike, partOf, trigger } = yield* call(
-    fetchRelated,
-    action
-  );
-}
+function* fetchExtas(action: AnyAction) {}
 
 export function* onGetRelated() {
   yield* takeLatest("word/fetchRelated", fetchExtas);
